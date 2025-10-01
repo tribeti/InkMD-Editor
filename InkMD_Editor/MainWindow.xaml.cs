3fa440c5157f57ca51af4c6748490696d63b3c2d
tribeti <117581928+tribeti@users.noreply.github.com>
2025-10-01 17:59:25 +0700

    add RichEditBox, add title bar, window custom

--- InkMD_Editor/MainWindow.xaml.cs
+++ InkMD_Editor/MainWindow.xaml.cs
@@ -1,31 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace InkMD_Editor
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
+            AppWindow.Resize(new Windows.Graphics.SizeInt32(1200 , 800));
             InitializeComponent();
+            AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+            AppWindow.Title = "InkMD Editor";
+        }
+
+        private void Menu_Opening(object sender, object e)
+        {
+            CommandBarFlyout myFlyout = sender as CommandBarFlyout;
+            if (myFlyout.Target == REBCustom)
+            {
+                AppBarButton myButton = new AppBarButton();
+                myFlyout.PrimaryCommands.Add(myButton);
+            }
+        }
+
+        private void REBCustom_Loaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening += Menu_Opening;
+            REBCustom.ContextFlyout.Opening += Menu_Opening;
+        }
+
+        private void REBCustom_Unloaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening -= Menu_Opening;
+            REBCustom.ContextFlyout.Opening -= Menu_Opening;
         }
     }
 }
