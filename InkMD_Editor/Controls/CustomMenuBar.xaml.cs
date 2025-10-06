4570ef6e8358f4f580c92d89eee1e9edd4ebbddb
tribeti <117581928+tribeti@users.noreply.github.com>
2025-10-06 17:16:26 +0700

    refactor code, scaffolding menu bar

---
+++ InkMD_Editor/Controls/CustomMenuBar.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace InkMD_Editor.Controls;
+
+public sealed partial class CustomMenuBar : UserControl
+{
+    public CustomMenuBar ()
+    {
+        InitializeComponent();
+    }
+}
