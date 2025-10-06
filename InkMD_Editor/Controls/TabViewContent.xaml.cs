806cceb3fe2b94992c10f12d3b298e33888365e5
tribeti <117581928+tribeti@users.noreply.github.com>
2025-10-06 23:37:13 +0700

    tab view, template content, almost done with main ui

---
+++ InkMD_Editor/Controls/TabViewContent.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace InkMD_Editor.Controls;
+
+public sealed partial class TabViewContent : UserControl
+{
+    public TabViewContent()
+    {
+        InitializeComponent();
+    }
+}
--- InkMD_Editor/EditorPage.xaml.cs
+++ InkMD_Editor/EditorPage.xaml.cs
@@ -1,3 +1,4 @@
+using InkMD_Editor.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
@@ -5,13 +6,56 @@ namespace InkMD_Editor;
 
 public sealed partial class EditorPage : Page
 {
-    public EditorPage()
+    public EditorPage ()
     {
         InitializeComponent();
+        Loaded += EditorPage_Loaded;
     }
 
     private void Button_Click (object sender , RoutedEventArgs e)
     {
         Frame.Navigate(typeof(SettingsPage));
+    }
+
+    private void EditorPage_Loaded (object sender , RoutedEventArgs e)
+    {
+        for ( int i = 0 ; i < 3 ; i++ )
+        {
+            Tabs.TabItems.Add(CreateNewTab(i));
+        }
+
+        if ( Tabs.TabItems.Count > 0 )
+        {
+            Tabs.SelectedIndex = 0;
+        }
+    }
+
+    private void TabView_AddButtonClick (TabView sender , object args)
+    {
+        var newTab = CreateNewTab(sender.TabItems.Count);
+        sender.TabItems.Add(newTab);
+    }
+
+    private void TabView_TabCloseRequested (TabView sender , TabViewTabCloseRequestedEventArgs args)
+    {
+        sender.TabItems.Remove(args.Tab);
+    }
+
+    private TabViewItem CreateNewTab (int index)
+    {
+        TabViewItem newItem = new TabViewItem
+        {
+            Header = $"Document {index}" ,
+            IconSource = new SymbolIconSource { Symbol = Symbol.Document }
+        };
+
+        var content = new TabViewContent
+        {
+            DataContext = $"Document {index}"
+        };
+
+        newItem.Content = content;
+
+        return newItem;
     }
 }
