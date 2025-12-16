c385155279f44b6f1507eed003d9ba7ac22fc97d
tribeti <117581928+tribeti@users.noreply.github.com>
2025-12-16 11:10:01 +0700

    new ui and func to setting page

---
+++ InkMD_Editor/Services/ThemeService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.UI.Xaml;
+using Windows.Storage;
+
+namespace InkMD_Editor.Services;
+
+public static class ThemeService
+{
+    private const string ThemeSettingKey = "AppTheme";
+
+    public enum AppTheme
+    {
+        Light = 0,
+        Dark = 1,
+        Default = 2
+    }
+
+    public static void SaveTheme (AppTheme theme)
+    {
+        ApplicationData.Current.LocalSettings.Values [ThemeSettingKey] = (int) theme;
+    }
+
+    public static AppTheme GetSavedTheme ()
+    {
+        if ( ApplicationData.Current.LocalSettings.Values.ContainsKey(ThemeSettingKey) )
+        {
+            return (AppTheme) (int) ApplicationData.Current.LocalSettings.Values [ThemeSettingKey];
+        }
+        return AppTheme.Default;
+    }
+
+    public static ElementTheme ToElementTheme (AppTheme theme)
+    {
+        return theme switch
+        {
+            AppTheme.Light => ElementTheme.Light,
+            AppTheme.Dark => ElementTheme.Dark,
+            AppTheme.Default => ElementTheme.Default,
+            _ => ElementTheme.Default
+        };
+    }
+
+    public static void ApplyTheme (Window window)
+    {
+        if ( window?.Content is FrameworkElement rootElement )
+        {
+            var savedTheme = GetSavedTheme();
+            rootElement.RequestedTheme = ToElementTheme(savedTheme);
+        }
+    }
+
+    public static void SetTheme (Window window , AppTheme theme)
+    {
+        if ( window?.Content is FrameworkElement rootElement )
+        {
+            SaveTheme(theme);
+            rootElement.RequestedTheme = ToElementTheme(theme);
+        }
+    }
+}
--- InkMD_Editor/Views/SettingsPage.xaml.cs
+++ InkMD_Editor/Views/SettingsPage.xaml.cs
@@ -1,3 +1,4 @@
+using InkMD_Editor.Services;
 using Microsoft.UI.Xaml.Controls;
 
 namespace InkMD_Editor.Views;
@@ -7,6 +8,26 @@ public sealed partial class SettingsPage : Page
     public SettingsPage ()
     {
         InitializeComponent();
+        LoadSavedTheme();
+    }
+
+    private void LoadSavedTheme ()
+    {
+        var savedTheme = ThemeService.GetSavedTheme();
+        ThemeComboBox.SelectedIndex = (int) savedTheme;
+    }
+
+    private void ThemeComboBox_SelectionChanged (object sender , SelectionChangedEventArgs e)
+    {
+        if ( ThemeComboBox.SelectedIndex == -1 )
+            return;
+
+        var selectedTheme = (ThemeService.AppTheme) ThemeComboBox.SelectedIndex;
+        var window = App.MainWindow;
+        if ( window is not null )
+        {
+            ThemeService.SetTheme(window , selectedTheme);
+        }
     }
 
     private void Button_Click (object sender , Microsoft.UI.Xaml.RoutedEventArgs e)
