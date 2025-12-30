6ac7aa411638c33af685f00615697708945f443c
tribeti <117581928+tribeti@users.noreply.github.com>
2025-12-30 22:17:31 +0700

    remove test proj

---
+++ InkMD_Editor/Services/IDialogService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace InkMD_Editor.Services;
+
+public interface IDialogService
+{
+    Task ShowErrorAsync (string message);
+    Task ShowSuccessAsync (string message);
+    Task<bool> ShowConfirmationAsync (string message);
+}
