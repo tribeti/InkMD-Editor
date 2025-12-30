0062cb74fde55776c05ab86cf646b506d60676a6
tribeti <117581928+tribeti@users.noreply.github.com>
2025-12-30 20:42:08 +0700

    .

---
+++ InkMD.Test/UnitTestAppWindow.xaml.cs
@@ -0,0 +1,13 @@
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace InkMD.Test
+{
+    public sealed partial class UnitTestAppWindow : Window
+    {
+        public UnitTestAppWindow ()
+        {
+            InitializeComponent();
+        }
+    }
+}
